487a4ff985766dd7b237551c5ed73c77a2c23114
Ahmed Abdelbary <ahmed.bary@icloud.com>
2022-06-11 21:15:22 +0200

    adding filter and allow dynamic rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,7 +8,10 @@ function Expenses(props) {
   const filterChangeHandler = (chosenYear) => {
     setReceivedYear(chosenYear);
   };
-  console.log(receivedYear);
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return receivedYear === expense.date.getFullYear().toString();
+  });
 
   return (
     <Card className="expenses">
@@ -16,26 +19,16 @@ function Expenses(props) {
         onFilterChange={filterChangeHandler}
         defaultYear={receivedYear}
       />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+      {filteredExpenses.length > 0 &&
+        filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
     </Card>
   );
 }
