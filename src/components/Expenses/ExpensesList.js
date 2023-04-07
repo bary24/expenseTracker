b00adda8fd5dbcf630b766e93bec54bf990787aa
bary24 <ahmed_abdelbary@mivors.com>
2023-04-07 13:01:40 +0200

    Fixing expense list rendering

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -2,25 +2,23 @@ import "../../styles/ExpensesList.css";
 import ExpenseItem from "./ExpenseItem";
 
 const ExpensesList = (props) => {
-  if (props.items.length === 0) {
-    return <h2 className="expenses-list__fallback"> No expenses found</h2>;
-  }
-
-  if (props.items.length > 0) {
-    // eslint-disable-next-line no-unused-vars
-     props.items.map((expense) => {
-      return  <ul  className="expenses-list">
-          <ExpenseItem
-          key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        </ul>
+    if (props.items.length === 0) {
+        return <h2 className="expenses-list__fallback"> No expenses found</h2>;
+    } else {
+        // eslint-disable-next-line no-unused-vars
+        return props.items.map((expense) => {
+            return (
+                <ul className="expenses-list">
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                </ul>
+            );
         });
-      
-    
-  }
+    }
 };
 
 export default ExpensesList;
